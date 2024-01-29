61cc68f6da1411137f694eda2379eb9040bfed6a
Mahasatya <79305118+Urstar3@users.noreply.github.com>
2024-01-29 19:15:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 apt update
+apt-get -y install apt-transport-https lsb-release ca-certificates curl
+wget -qO - https://modsecurity.digitalwave.hu/archive.key | sudo apt-key add -
+ sudo sh -c 'echo "deb http://modsecurity.digitalwave.hu/ubuntu/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/dwmodsec.list'
+ sudo sh -c 'echo "deb http://modsecurity.digitalwave.hu/ubuntu/ $(lsb_release -sc)-backports main" >> /etc/apt/sources.list.d/dwmodsec.list'
+cat << EOF | sudo tee -a /etc/apt/preferences.d/99modsecurity
+Package: *libnginx-mod-http-modsecurity*
+Pin: origin modsecurity.digitalwave.hu
+Pin-Priority: 900
+
+Package: *libapache2-mod-security2*
+Pin: origin modsecurity.digitalwave.hu
+Pin-Priority: 900
+
+Package: *modsecurity-crs*
+Pin: origin modsecurity.digitalwave.hu
+Pin-Priority: 900
+
+Package: *libmodsecurity*
+Pin: origin modsecurity.digitalwave.hu
+Pin-Priority: 900
+EOF
+apt update
 apt install snort apache2 php libapache2-mod-security2 libxml2 libxml2-dev libxml2-utils libaprutil1 policycoreutils selinux-utils selinux-basics libaprutil1-dev libapache2-mod-evasive
 selinux-activate
 sudo mkdir /var/log/mod_evasive
