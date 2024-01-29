f8ed8940bd8fbcbd574c2acf685b6ec67de64479
Mahasatya <79305118+Urstar3@users.noreply.github.com>
2024-01-29 17:46:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ apt update
 apt install snort apache2 php libapache2-modsecurity libxml2 libxml2-dev libxml2-utils libaprutil1 policycoreutils selinux-utils selinux-basics libaprutil1-dev libapache2-mod-evasive
 selinux-activate
 sudo mkdir /var/log/mod_evasive
+mv /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
 sudo chown www-data:www-data /var/log/mod_evasive/
 mkdir /etc/apache2/modsec/
 wget https://github.com/coreruleset/coreruleset/archive/refs/tags/v3.3.4.tar.gz
